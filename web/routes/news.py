7fe2a2d2518fe93fd60486a16b7a552008636e99
ciaqui <ciara@u1trav101.net>
2023-08-11 21:12:47 +0100

    '/news' shows friend blogposts

--- web/routes/news.py
+++ web/routes/news.py
@@ -1,10 +1,19 @@
+from flask import request, session, redirect, url_for
 from web.misc import render_template
 from db import Query
 
 
 def news():
     query = Query()
-    blogs = query.get_all_blogposts(20)
+    blogs = None
+
+    if request.args.get("show") == "friends":
+        if ("user_id") not in session:
+            return redirect(url_for("news"))
+        
+        blogs = query.get_all_friend_blogposts(session["user_id"])
+    else:
+        blogs = query.get_all_blogposts(20)
     
     return render_template(
         "news.html",
