baed791de54fd1d4f877049ff378daa2288e60b4
ciaqui <ciara@u1trav101.net>
2023-10-14 22:33:41 +0100

    fixed conflicting friend requests bug

--- web/routes/remove_friend.py
+++ web/routes/remove_friend.py
@@ -1,4 +1,4 @@
-from flask import session, redirect, url_for
+from flask import session, redirect, url_for, request
 from web.forms import friend_form
 from db import Query
 
@@ -18,3 +18,9 @@ def remove_friend(user_id):
         query = Query()
         query.friend_reject_action(session["user_id"], user_id)
         query.friend_reject_action(user_id, session["user_id"])
+    
+    print(request.referrer)
+    if request.referrer is None or url_for("remove_friend", user_id=user_id) in request.referrer:
+        return redirect(url_for("friend_list", user_id=session["user_id"]))
+    
+    return redirect(request.referrer)
