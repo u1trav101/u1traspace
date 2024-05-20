2e569d7c4e2512bbe9661546768547fcaa42f678
ducksabduct <ciara@u1trav101.net>
2024-05-20 02:55:47 +0100

    removed unneed script

--- apt-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This project depends on python v3.10 or later
-# These commands apply for debian 11
-# - libmariadb-dev in the debian repos is too out of date, add the mariadb repo beforehand
-# - redis must be installed through the redis repository
-
-sudo apt install libjpeg-dev zlib1g-dev mariadb-server libmariadb3 libmariadb-dev mariadb-server ffmpeg redis
