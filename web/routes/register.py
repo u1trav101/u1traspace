5ab69903a00da23cf838078f4701552587945818
ducksabduct <ciara@u1trav101.net>
2024-05-20 19:33:15 +0100

    removed flask_simple_captcha

--- web/routes/register.py
+++ web/routes/register.py
@@ -5,27 +5,21 @@ import web.forms as forms
 import auth
 
 
-def register(captcha):
+def register():
     if ("user_id") in session:
         return redirect(url_for("user_profile", user_id=session["user_id"]))
 
     register_form = forms.register_form()
-    captcha_obj = captcha.create()
 
     if register_form.validate_on_submit():
-        captcha_hash = request.form.get("captcha-hash")
-        captcha_text = request.form.get("captcha-text")
+        # registration successful
+        user_id = auth.register_user(register_form.username.data, register_form.password.data)
 
-        if captcha.verify(captcha_text, captcha_hash):
-            # registration successful
-            user_id = auth.register_user(register_form.username.data, register_form.password.data)
+        copy_default_avatar.delay(user_id)
 
-            copy_default_avatar.delay(user_id)
-
-            return forms.register_commit(register_form.username.data, user_id)
+        return forms.register_commit(register_form.username.data, user_id)
 
     return render_template(
         "register.html",
         form=register_form,
-        captcha=captcha_obj
     )
