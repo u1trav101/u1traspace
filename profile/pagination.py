7880859f465efd88fb2eb93023fb283a8b3a01c0
ducksabduct <ciara@u1trav101.net>
2024-06-07 02:13:27 +0100

    users page has sort and filter options

--- profile/pagination.py
+++ profile/pagination.py
@@ -6,14 +6,27 @@ from db import Query
 def users_paginator():
     query = Query()
 
-    filter_online = True if request.args.get("show") == "online" else False
-
-    number_of_users = query.select_users(count=True)
+    filter_online = True if request.args.get("f") == "online" else False
+    sort = request.args.get("s")
+    
+    number_of_users = query.select_users(count=True, online=filter_online)
 
     page_number = get_page_args(page_parameter="page")[0]
     users_per_page = 15
     offset = (page_number * 15) - 15
-    users = query.select_users(online=filter_online, start=offset)
+
+    users = []
+    match sort:
+        case "new":
+            users = query.select_users(online=filter_online, start=offset)
+        case "old":
+            users = query.select_users(online=filter_online, start=offset, order="ASC")
+        case "mviews":
+            users = query.select_users(online=filter_online, start=offset, order_by="page_views")
+        case "lviews":
+            users = query.select_users(online=filter_online, start=offset, order_by="page_views", order="ASC")
+        case _:
+            users = query.select_users(online=filter_online, start=offset)
 
     pagination = Pagination(
         page=page_number,
