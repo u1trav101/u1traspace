2d0e539105a30777389ce849dbe052f7229a372e
ducksabduct <ciara@u1trav101.net>
2024-06-27 04:06:33 +0100

    started index page

--- web/routes/index.py
+++ web/routes/index.py
@@ -1,4 +1,7 @@
+from random import randint, seed
+from datetime import date
 from web.misc import render_template
+from profile.properties import get_profile_properties
 import web.forms as forms
 from db import Query
 
@@ -9,8 +12,14 @@ def index():
 
     search_form = forms.search_form()
 
+    number_of_users = query.select_users(count=True)
+    seed(str(date.today()))
+    user_of_the_day = randint(0, number_of_users)
+
     return render_template(
         "index.html",
         new_posts=res,
-        form=search_form
+        form=search_form,
+        user_of_the_day=user_of_the_day,
+        user=get_profile_properties(user_of_the_day)
     )
