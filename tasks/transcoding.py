26d3bc0a6b621c247597f3ab34d19da3bb7c8398
ducksabduct <ciara@u1trav101.net>
2024-07-14 20:08:35 +0200

    uploaded avatars preserve infinite loop

--- tasks/transcoding.py
+++ tasks/transcoding.py
@@ -5,7 +5,7 @@ import os
 
 @shared_task()
 def _transcode_and_upload_image(file_path, usercontent_dir, user_id, size):
-    os.system(f"ffmpeg -loglevel error -hide_banner -i {file_path} -y -vf scale={size}:{size} {usercontent_dir}img/rsz/{size}px/{user_id}.webp")
+    os.system(f"ffmpeg -loglevel error -hide_banner -loop -1 -i {file_path} -y -vf scale={size}:{size} {usercontent_dir}img/rsz/{size}px/{user_id}.webp")
     cdn.upload_image(
         f"{usercontent_dir}img/rsz/{size}px/{user_id}.webp",
         f"u1traspace/usercontent/img/rsz/{size}px/{user_id}.webp"
@@ -18,7 +18,7 @@ def transcode_and_upload_images(file_path, usercontent_dir, user_id):
     _transcode_and_upload_image.delay(file_path, usercontent_dir, user_id, 100)
     _transcode_and_upload_image.delay(file_path, usercontent_dir, user_id, 200)
 
-    os.system(f"ffmpeg -loglevel error -hide_banner -i {file_path} -y {usercontent_dir}img/raw/{user_id}.webp")
+    os.system(f"ffmpeg -loglevel error -hide_banner -loop -1 -i {file_path} -y {usercontent_dir}img/raw/{user_id}.webp")
     cdn.upload_image(
         f"{usercontent_dir}img/raw/{user_id}.webp",
         f"u1traspace/usercontent/img/raw/{user_id}.webp"
@@ -27,7 +27,7 @@ def transcode_and_upload_images(file_path, usercontent_dir, user_id):
 
 @shared_task()
 def transcode_and_upload_audio(file_path, usercontent_dir, user_id):
-    os.system(f"ffmpeg -loglevel error -i {file_path} -y {usercontent_dir}audio/{user_id}.mp3")
+    os.system(f"ffmpeg -loglevel error -loop -1 -i {file_path} -y {usercontent_dir}audio/{user_id}.mp3")
     cdn.upload_audio(
         f"{usercontent_dir}audio/{user_id}.mp3",
         f"u1traspace/usercontent/audio/{user_id}.mp3"
