49eccf37b5473acc5cad61fb27ff87b264a5fd0f
ciaqui <ciara@u1trav101.net>
2024-08-23 19:20:24 +0200

    fixed blueprints __init__

---
+++ web/blueprints/__init__.py
@@ -0,0 +1,4 @@
+from web.blueprints.auth import auth_blueprint
+from  web.blueprints.blog import blog_blueprint 
+from web.blueprints.message import message_blueprint
+from web.blueprints.user import user_blueprint
