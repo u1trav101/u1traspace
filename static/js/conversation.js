8ee252923c5f4b7b6cd73bf03a86a18a269a16b1
ciaqui <ciara@u1trav101.net>
2024-08-27 23:16:36 +0200

    messaging now stable

--- static/js/conversation.js
+++ static/js/conversation.js
@@ -1,15 +1,20 @@
 let socket = null;
-let messages = [];
+let messages = {};
 let messagesDiv = null;
 let renderedMessages = 0;
+let lastMessageIDs = {};
+let recipientID = null;
+
 
 const createSocket = recipientID => {
+    userID = recipientID
     if (socket) { 
         if (socket.url.includes(`/msg/${recipientID}`)) return;
-        else socket.close();
+        else {
+            socket.close()
+            console.log("Closed current socket connection")
+        };
     }
-
-    console.log("Closed current socket connection")
 
     messages = [];
     messagesDiv = document.getElementById(`messages-${recipientID}`)
@@ -62,7 +67,9 @@ const receiveMessage = event => {
                 } else {
                     messages.push(msg.data);
                 }
+
                 renderMessages();
+                lastMessageIDs[userID] = messages[messages.length - 1]["message_id"]
 
                 break;
             
@@ -74,43 +81,45 @@ const receiveMessage = event => {
 
 const renderMessages = () => {
     for (let i = renderedMessages; i < messages.length; i++) {
-        console.log("Rendering...");
+        if ((!lastMessageIDs[userID]) || messages[i]["message_id"] > lastMessageIDs[userID]) {
+            console.log("Rendering...");
         
-        // creating new elements
-        const newMessage = document.createElement("div");
-        const avatar = document.createElement("img");
-        avatar.setAttribute("src", `https://cdn.u1trav101.net/u1traspace/usercontent/img/rsz/100px/${messages[i]["user_id"]}.webp`)
-        const top = document.createElement("span");
-        const right = document.createElement("div");
-        const author = document.createElement("h4");
-        const date = document.createTextNode(`at ${messages[i]["date"]}`);
-        author.innerText = messages[i]["username"];
-        const corpus = document.createElement("p");
-        corpus.innerText = messages[i]["corpus"];
-        
-        // applying classes to new elements
-        avatar.classList = "avatar";
-        newMessage.classList = "message";
-        top.classList = "top";
-        right.classList = "right"
-        author.classList = "username";
-        corpus.classList = "corpus";
-
-        // organising new element tree
-        top.appendChild(author);
-        top.appendChild(date);
-        right.appendChild(top)
-        right.appendChild(corpus)
-        newMessage.appendChild(avatar);
-        newMessage.appendChild(right);
-
-        // rendering element
-        messagesDiv.appendChild(newMessage);
-
-        // incrementing counter of rendered messages
-        renderedMessages++;
-
-        // scrolling to bottom
-        messagesDiv.scrollTo(0, messagesDiv.scrollHeight);
+            // creating new elements
+            const newMessage = document.createElement("div");
+            const avatar = document.createElement("img");
+            avatar.setAttribute("src", `https://cdn.u1trav101.net/u1traspace/usercontent/img/rsz/100px/${messages[i]["user_id"]}.webp`)
+            const top = document.createElement("span");
+            const right = document.createElement("div");
+            const author = document.createElement("h4");
+            const date = document.createTextNode(`at ${messages[i]["date"]}`);
+            author.innerText = messages[i]["username"];
+            const corpus = document.createElement("p");
+            corpus.innerText = messages[i]["corpus"];
+            
+            // applying classes to new elements
+            avatar.classList = "avatar";
+            newMessage.classList = "message";
+            top.classList = "top";
+            right.classList = "right"
+            author.classList = "username";
+            corpus.classList = "corpus";
+    
+            // organising new element tree
+            top.appendChild(author);
+            top.appendChild(date);
+            right.appendChild(top)
+            right.appendChild(corpus)
+            newMessage.appendChild(avatar);
+            newMessage.appendChild(right);
+    
+            // rendering element
+            messagesDiv.appendChild(newMessage);
+    
+            // incrementing counter of rendered messages
+            renderedMessages++;
+    
+            // scrolling to bottom
+            messagesDiv.scrollTo(0, messagesDiv.scrollHeight);
+        }
     }
 }
