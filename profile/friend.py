9bc40e516158c1e8957be80317cefd0a486a3685
ciaqui <ciara@u1trav101.net>
2024-09-16 02:50:30 +0100

    fix: various bugs around adding and removing friends

--- profile/friend.py
+++ profile/friend.py
@@ -39,10 +39,10 @@ def send_friend_request(sender_id: int, recipient_id: int) -> bool:
     )
     return False
 
-def get_user_friends(user_id: int) -> list[dict] | None:
+def get_user_friends(user_id: int, approved:bool|None = True) -> list[dict] | None:
     query = Query()
 
-    res: list = query.select_friends(sender_id=user_id) + query.select_friends(recipient_id=user_id)
+    res: list = query.select_friends(sender_id=user_id, approved=approved) + query.select_friends(recipient_id=user_id, approved=approved)
 
     if not res:
         return None
@@ -62,14 +62,14 @@ def get_friend_requests(user_id: int) -> list[dict] | None:
     
     return res
     
-def is_friends(user_id: int) -> bool:
+def is_friends(user_id: int) -> bool | str:
     if ("user_id") not in session:
         return False
     
-    friends = get_user_friends(user_id)
+    friends = get_user_friends(user_id, None)
     if friends:
         for friend in friends:
             if (friend["sender_id"] == int(session["user_id"])) or (friend["recipient_id"] == int(session["user_id"])):
-                return True
+                return True if friend["approved"] is True else "pending"
     
     return False
