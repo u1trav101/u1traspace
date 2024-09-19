6bb78f09324a452b872d312c39b4aeeb9db26e79
ciaqui <ciara@u1trav101.net>
2024-09-19 22:54:20 +0100

    new: added logging

---
+++ src/log/write.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from datetime import datetime
+
+
+LOG_PATH = "../logs/u1traspace.log"
+
+
+def write(module: str, msg: str) -> None:
+    number_of_lines: int = 0
+    time: datetime = datetime.now()
+    time_str: str = datetime.strftime(time, "%d/%m/%y %H:%M:%S")
+
+    try:
+        with open(Path(LOG_PATH).resolve(), "r") as log:
+            number_of_lines = len(log.readlines())
+    except FileNotFoundError:
+        pass
+
+    with open(Path(LOG_PATH).resolve(), "a") as log:
+        log.write(
+            f"{'\n' if number_of_lines != 0 else ''}({time_str}) [{module}] {msg}"
+        )
--- src/tasks/sync.py
+++ src/tasks/sync.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from datetime import datetime, timezone
 from os.path import getmtime
 from cdn import upload
+import log
 
 static_files: list[str] = []
 
@@ -20,10 +21,13 @@ def sync_folder(path: str, recursive: bool = False) -> None:
 
 
 def sync_static():
-    print("Syncing static files with CDN")
+    log.write(__name__, "syncing static files with cdn...")
 
+    log.write(__name__, "checking modification dates of cdn static files...")
     sync_folder("u1traspace/static/css/", True)
+    log.write(__name__, "checking modification dates of cdn static files... DONE")
 
+    log.write(__name__, "checking modification dates of local static files...")
     for file in static_files:
         local_file = Path("static/" + file.get("Key")[18:])
         local_mtime = datetime.fromtimestamp(getmtime(local_file.resolve())).astimezone(
@@ -33,8 +37,17 @@ def sync_static():
         remote_mtime = file.get("LastModified")
 
         if local_mtime.timestamp() > remote_mtime.timestamp():
-            print(f"{file.get("Key")} is out of date. Syncing...")
-            upload(local_file.resolve(), file.get("Key"))
-            print(f"{file.get("Key")} synced.")
+            log.write(
+                __name__,
+                f"{file.get("Key")} is out of date. uploading local version...",
+            )
 
-    print("DONE")
+            upload(local_file.resolve(), file.get("Key"))
+
+            log.write(
+                __name__,
+                f"{file.get("Key")} is out of date. uploading local version... DONE",
+            )
+
+    log.write(__name__, "checking modification dates of local static files... DONE")
+    log.write(__name__, "syncing static files with cdn... DONE")
